fcbad84f0f16f11920c31f68549a805a2a0d0082
Lee Capps <himself@leecapps.com>
2012-07-18 10:27:17 -0400

    Document Verso and Verso::Base

--- lib/verso/base.rb
+++ lib/verso/base.rb
@@ -1,4 +1,15 @@
+# A wrapper for the Virginia CTE Resource Center's Web API, providing access
+# to Virginia's Career and Technical Education curriculum, course, and
+# occupation data.
+#
+# @see http://api.cteresource.org/docs Web API documentation
 module Verso
+  # Classes derived from Verso::Base should define attr_readers or
+  # appropriate methods (using Verso::Base#get_attr w/in methods) to access
+  # attributes stored in the attrs hash.
+  #
+  # @!attribute [r] attrs
+  #   attribute hash
   class Base
     attr_reader :attrs
     alias to_hash attrs
@@ -26,6 +37,12 @@ module Verso
 
   protected
 
+    # Get attribute value from attrs attribute store or raise NoMethodError
+    # if the key is not defined.
+    #
+    # @param attr [Symbol]
+    # @return attribute value
+    # @raise [NoMethodError]
     def get_attr(attr)
       attrs.has_key?(attr) ? attrs[attr] : method_missing(attr)
     end
