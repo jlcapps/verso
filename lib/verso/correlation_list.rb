78ea5d5f106181c53f5ef2194a27b558ef388929
Lee Capps <himself@leecapps.com>
2012-07-18 14:48:48 -0400

    Document Verso::CorrelationList

--- lib/verso/correlation_list.rb
+++ lib/verso/correlation_list.rb
@@ -1,4 +1,22 @@
 module Verso
+  # Correlation List Resource
+  #
+  # A crosswalk of competencies to standards body goals.
+  # @see http://api.cteresource.org/docs/courses/course/standards/standard/correlations
+  #
+  # @!attribute [r] code
+  #   @return [String] Course code
+  # @!attribute [r] edition
+  #   @return [String] Edition year
+  # @!attribute [r] name
+  #   @return [String] Standards Body name, used as an identifier and not to
+  #     be confused with is title
+  #
+  # @overload initialize(attrs={})
+  #   @note Certain attributes are required for instantiation.
+  #   @option attrs [String] :code Course code *Required*
+  #   @option attrs [String] :edition Edition year *Required*
+  #   @option attrs [String] :name Standards Body name *Required*
   class CorrelationList < Verso::Base
     include Enumerable
     include HTTPGettable
@@ -6,6 +24,7 @@ module Verso
     def_delegators :correlations, :[], :each, :empty?, :last, :length
     attr_reader :code, :edition, :name
 
+    # @return [String] Standards body title
     def title
       @title ||= first.goals.first.title
     end
