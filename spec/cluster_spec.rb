5acd8a350578bd52778a883872c5ff9915aaf8b4
Lee Capps <himself@leecapps.com>
2012-07-18 12:21:26 -0400

    Add full examples for Verso::Cluster

--- spec/cluster_spec.rb
+++ spec/cluster_spec.rb
@@ -1,18 +1,115 @@
 require 'spec_helper'
 
 describe Verso::Cluster do
-  before do
-    @pathway = { "title" => "Agribusiness Systems", "id" => 306 }
-    @raw_cluster = { "title" => "Information Technology",
-                     "pathways" => [@pathway] }
-    @cluster = Verso::Cluster.new(@raw_cluster)
+  use_vcr_cassette :record => :new_episodes
+
+  before(:each) do
+    @cluster = Verso::ClusterList.new.first
   end
 
-  it 'should respond to title' do
-    @cluster.title.should eq("Information Technology")
+  describe '#code' do
+    it 'responds' do
+      @cluster.should respond_to(:code)
+    end
+
+    it 'is String' do
+      @cluster.code.should be_a(String)
+    end
+
+    it 'is formatted like CT1, CT2, etc.' do
+      @cluster.code.should match(/CT\d+/)
+    end
   end
 
-  it 'should respond to pathways' do
-    @cluster.pathways.first.title.should eq("Agribusiness Systems")
+  describe '#contact' do
+    it 'responds' do
+      @cluster.should respond_to(:contact)
+    end
+
+    it 'is an OpenStruct' do
+      @cluster.contact.should be_a(OpenStruct)
+    end
+
+    it 'has name' do
+      @cluster.contact.name.should be_a(String)
+    end
+
+    it 'has email' do
+      @cluster.contact.email.should match(/\w+@doe.virginia.gov/)
+    end
+
+    it 'has phone' do
+      @cluster.contact.phone.should match(/\d{3}-\d{3}-\d{4}/)
+    end
+  end
+
+  describe '#description' do
+    it 'responds' do
+      @cluster.should respond_to(:description)
+    end
+
+    it 'is String' do
+      @cluster.description.should be_a(String)
+    end
+
+    it 'is HTML' do
+      @cluster.description.should match(/<\/.+>/)
+    end
+  end
+
+  describe '#id' do
+    it 'responds' do
+      @cluster.should respond_to(:id)
+    end
+
+    it 'is Fixnum' do
+      @cluster.id.should be_a(Fixnum)
+    end
+
+    it 'is not the same as #object_id' do
+      @cluster.id.should_not == @cluster.object_id
+    end
+  end
+
+  describe '#pathways' do
+    it 'responds' do
+      @cluster.should respond_to(:pathways)
+    end
+
+    it 'is Array' do
+      @cluster.pathways.should be_a(Array)
+    end
+
+    it 'includes a Pathway' do
+      @cluster.pathways.first.should be_a(Verso::Pathway)
+    end
+  end
+
+  describe '#postsecondary_info' do
+    it 'responds' do
+      @cluster.should respond_to(:postsecondary_info)
+    end
+
+    it 'is String' do
+      @cluster.postsecondary_info.should be_a(String)
+    end
+
+    it 'is HTML' do
+      @cluster.postsecondary_info.should match(/<\/.+>/)
+    end
+  end
+
+  describe '#title' do
+    it 'responds' do
+      @cluster.should respond_to(:title)
+    end
+
+    it 'returns String' do
+      @cluster.title.should be_a(String)
+    end
+
+    it 'returns correct title' do
+      @cluster.title.should == 'Agriculture, Food and Natural Resources'
+    end
   end
 end
