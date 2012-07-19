585c39824b45ac0a2784d3f085142687eb655725
Lee Capps <himself@leecapps.com>
2012-07-19 14:47:38 -0400

    Add Verso::Extra examples

--- spec/extra_spec.rb
+++ spec/extra_spec.rb
@@ -1,36 +1,75 @@
 require 'spec_helper'
 
 describe Verso::Extra do
-  before do
-    Net::HTTP.stub(:get).
-      with('api.cteresource.org',
-           "/courses/6320,2011/extras/untitled~6320",
-           80).
-      and_return(JSON.pretty_generate(
-        :extra => { :title => "Instructional Scenario",
-                    :description => "extra description" }
-      ))
-    @extra = Verso::Extra.new("code" => "6320", "edition" => "2011",
-                       "name" => "untitled~6320",
-                       "title" => "Instructional Scenarios")
+  use_vcr_cassette :record => :new_episodes
+
+  before(:each) do
+    @extra = Verso::CourseList.new(:code => "6320").last.extras.first
   end
 
-  it "responds to #title" do
-    @extra.title.should eq("Instructional Scenarios")
+  describe '#code' do
+    it 'responds' do
+      @extra.should respond_to(:code)
+    end
+
+    it 'is a String' do
+      @extra.code.should be_a(String)
+    end
+
+    it 'looks like a code' do
+      @extra.code.should match(/\d{4}/)
+    end
   end
 
-  it "responds to #description" do
-    @extra.description.should eq("extra description")
-  end
-  it "responds to #code" do
-    @extra.code.should eq("6320")
+  describe '#description' do
+     it 'responds' do
+       @extra.should respond_to(:description)
+     end
+
+     it 'is a String' do
+       @extra.description.should be_a(String)
+     end
+
+     it 'looks like HTML' do
+       @extra.description.should match(/<\/.+>/)
+     end
   end
 
-  it "resonds to #name" do
-    @extra.name.should eq("untitled~6320")
+  describe '#edition' do
+    it 'responds' do
+      @extra.should respond_to(:edition)
+    end
+
+    it 'is a String' do
+      @extra.edition.should be_a(String)
+    end
+
+    it 'looks like a year' do
+      @extra.edition.should match(/2\d{3}/)
+    end
   end
 
-  it "responds to #edition" do
-    @extra.edition.should eq("2011")
+  describe '#name' do
+    it 'responds' do
+      @extra.should respond_to(:name)
+    end
+
+    it 'is a String' do
+      @extra.name.should be_a(String)
+    end
+
+    it 'looks like a name (i.e., untitled~6320)' do
+      @extra.name.should match(/^[a-z_~\d]+$/)
+    end
+  end
+
+  describe '#title' do
+    it 'responds' do
+      @extra.should respond_to(:title)
+    end
+
+    it 'is a String' do
+      @extra.title.should be_a(String)
+    end
   end
 end
