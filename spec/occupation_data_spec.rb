93f3c087ec53f1b310ff68fd783e20de99a1939c
Lee Capps <himself@leecapps.com>
2012-07-19 17:14:56 -0400

    Add Verso::OccupationData examples

--- spec/occupation_data_spec.rb
+++ spec/occupation_data_spec.rb
@@ -1,24 +1,42 @@
 require 'spec_helper'
 
 describe Verso::OccupationData do
-  before do
-    @occupation_data = [{
-    "cluster" => { "title" => "Information Technology" },
-    "pathway" => { "title" => "Information and Support Services" },
-    "occupations" => [{ "title" => "Database Analyst" }]
-    }]
-    @occdata = Verso::OccupationData.new(@occupation_data.first)
+  use_vcr_cassette :record => :new_episodes
+
+  before(:each) do
+    @od = Verso::OccupationList.new(:text => "golf").first
+  end
+  describe '#cluster' do
+    it 'responds' do
+      @od.should respond_to(:cluster)
+    end
+
+    it 'is a Verso::Cluster' do
+      @od.cluster.should be_a(Verso::Cluster)
+    end
   end
 
-  it 'should respond to title' do
-    @occdata.cluster.title.should eq("Information Technology")
+  describe '#pathway' do
+    it 'responds' do
+      @od.should respond_to(:pathway)
+    end
+
+    it 'is a Verso::Pathway' do
+      @od.pathway.should be_a(Verso::Pathway)
+    end
   end
 
-  it 'should respond to pathway' do
-    @occdata.pathway.title.should eq("Information and Support Services")
-  end
+  describe '#occupations' do
+    it 'responds' do
+      @od.should respond_to(:occupations)
+    end
 
-  it 'should respond to occupations' do
-    @occdata.occupations.first.title.should eq("Database Analyst")
+    it 'is an Array' do
+      @od.occupations.should be_a(Array)
+    end
+
+    it 'is an Array of Verso::Occupation objects' do
+      @od.occupations.first.should be_a(Verso::Occupation)
+    end
   end
 end
