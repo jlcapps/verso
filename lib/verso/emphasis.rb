bb5727d517599dfc98853e966dfd94e314a2c7cd
Lee Capps <himself@leecapps.com>
2012-07-19 13:42:41 -0400

    Add Verso::Emphasis documentation

--- lib/verso/emphasis.rb
+++ lib/verso/emphasis.rb
@@ -1,8 +1,22 @@
 module Verso
+  # Academic Emphasis resource
+  #
+  # @see http://api.cteresource.org/docs/academics/emphasis
+  #
+  # @!attribute [r] id
+  #   @return [Fixnum] Academic Emphasis id
+  # @!attribute [r] title
+  #   @return Academic Emphasis title
+  #
+  # @overload initialize(attrs={})
+  #   @note Any attributes may be set upon instantiation, using Options Hash.
+  #     The following are required:
+  #   @option attrs [Fixnum] :id Academic Emphasis id *Required*
   class Emphasis < Verso::Base
     include HTTPGettable
     attr_reader :id, :title
 
+    # @return [Array] Collection of related {Verso::OccupationData} objects
     def occupation_data
       @occupation_data ||= get_attr(:occupation_data).
         collect { |od| OccupationData.new(od) }
