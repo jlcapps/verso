69783b8e480000be9de34078f8dead63a5b123e6
Lee Capps <himself@leecapps.com>
2012-07-19 13:48:09 -0400

    Delegate array-like behavior

--- lib/verso/emphasis_list.rb
+++ lib/verso/emphasis_list.rb
@@ -2,20 +2,14 @@ module Verso
   class EmphasisList < Verso::Base
     include Enumerable
     include HTTPGettable
+    extend Forwardable
+    def_delegators :emphases, :[], :each, :empty?, :last, :length
+
+  private
 
     def emphases
       @emphases ||= get_attr(:emphases).collect { |em| Emphasis.new(em) }
     end
-
-    def each &block
-      emphases.each &block
-    end
-
-    def last
-      emphases[-1]
-    end
-
-  private
 
     def path
       "/academics/"
