19bc7d596f808c4ac30a8706b3bf5845f61abf53
Lee Capps <himself@leecapps.com>
2012-07-19 16:11:09 -0400

    alpha order

--- lib/verso/occupation.rb
+++ lib/verso/occupation.rb
@@ -3,13 +3,13 @@ module Verso
     include HTTPGettable
     attr_reader :description, :id, :title
 
+    def pathway
+      @pathway ||= Pathway.new(get_attr(:pathway))
+    end
+
     def related_courses
       @related_courses ||= get_attr(:related_courses).
                              collect { |c| Course.new(c) }
-    end
-
-    def pathway
-      @pathway ||= Pathway.new(get_attr(:pathway))
     end
 
   private
