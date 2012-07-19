17fbea289cf535d7e278f5c00663937c5b4ca4de
Lee Capps <himself@leecapps.com>
2012-07-19 12:55:41 -0400

    Add Verso::DutyArea documentation

--- lib/verso/duty_area.rb
+++ lib/verso/duty_area.rb
@@ -1,7 +1,26 @@
 module Verso
+  # Duty Area
+  #
+  # A group of tasks in a {Verso::TaskList}.
+  #
+  # @see http://api.cteresource.org/docs/courses/course/tasks
+  #
+  # @!attribute [r] code
+  #   @return [String] Course code
+  # @!attribute [r] edition
+  #   @return [String] Course edition year
+  # @!attribute [r] title
+  #   @return [String] Duty Area title
+  #
+  # @note A DutyArea is created for you by {Verso::TaskList}. It corresponds
+  #   to a portion of the Task List resource. You should never need to
+  #   instantiate one yourself.
   class DutyArea < Verso::Base
     attr_reader :code, :edition, :title
 
+    # Tasks within this Duty Area
+    #
+    # @return [Array] {Verso::Task} objects
     def tasks
       @tasks ||= get_attr(:tasks).collect do |t|
         Task.new(t.merge(:code => code, :edition => edition))
