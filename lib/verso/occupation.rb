da201f21067bf441421a371634933a7e4f760a87
Lee Capps <himself@leecapps.com>
2012-07-19 16:37:26 -0400

    Add Verso::Occupation documentation

--- lib/verso/occupation.rb
+++ lib/verso/occupation.rb
@@ -1,12 +1,34 @@
 module Verso
+  # Occupation resource
+  #
+  # An occupation is contained by a Pathway
+  #
+  # @see http://api.cteresource.org/docs/occupations/occupation
+  # @see http://www.careertech.org/career-clusters/clusters-occupations.html
+  #
+  # @!attribute [r] description
+  #   @return [String] HTML-formatted Occupation description
+  # @!attribute [r] id
+  #   @return [Fixnum] Occupation ID
+  # @!attribute [r] preparations
+  #   @return [Array] Collection of preparation titles (strings)
+  # @!attribute [r] title
+  #   @return [String] Occupation title
+  #
+  # @overload initialize(attrs={})
+  #   @note Any attributes may be set upon instantiation, using Options Hash.
+  #     The following are required:
+  #   @option attrs [Fixnum] :id Occupation ID *Required*
   class Occupation < Verso::Base
     include HTTPGettable
     attr_reader :description, :id, :preparations, :title
 
+    # @return [Verso::Pathway] Pathway that is the parent of this occupation
     def pathway
       @pathway ||= Pathway.new(get_attr(:pathway))
     end
 
+    # @return [Array] Collection of related {Verso::Course} objects
     def related_courses
       @related_courses ||= get_attr(:related_courses).
                              collect { |c| Course.new(c) }
