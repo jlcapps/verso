f6bd1ce24286f0b7bee87003d71f8d76b172c83e
Lee Capps <himself@leecapps.com>
2012-07-19 14:31:21 -0400

    Add Verso::ExaminationList documentation

--- lib/verso/examination_list.rb
+++ lib/verso/examination_list.rb
@@ -1,4 +1,22 @@
 module Verso
+  # Examination List resource
+  #
+  # A collection of Examination stand-in objects that respond to:
+  # * #amt_seal @return [Boolean] AMT Seal
+  # * #passing_score @return [String] Passing score
+  # * #retired @return [Boolean] Slated to be deleted?
+  # * #source @return [String] Title of exam source
+  # * #title @return [String] Exam title
+  # * #verified_credit @return [Booelean] Verified credit
+  #
+  # The attributes of the Examination stand-ins are similar to
+  # {Verso::Credential}.
+  #
+  # @example Get the list
+  #   exams = Verso::ExaminationList.new # => everything
+  #   exams.first.title # => "Advanced Placement Computer Science A"
+  #
+  # @see http://api.cteresource.org/docs/examinations
   class ExaminationList < Verso::Base
     include Enumerable
     include HTTPGettable
