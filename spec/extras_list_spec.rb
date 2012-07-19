20e3b0554ed2cdd3e9eda91b323011c1e0a8bc56
Lee Capps <himself@leecapps.com>
2012-07-19 15:03:02 -0400

    Add Verso::ExtrasList examples

--- spec/extras_list_spec.rb
+++ spec/extras_list_spec.rb
@@ -1,26 +1,88 @@
 require 'spec_helper'
 
 describe Verso::ExtrasList do
-  before do
-    @extra = double("Verso::Extra", :code => "6320", :edition => "2011",
-                    :name => "untitled~6320",
-                    :title => "Instructional Scenarios")
-    @extras_list = Verso::ExtrasList.new(:code => "6320", :edition => "2011")
-    Net::HTTP.stub(:get).and_return(
-      JSON.pretty_generate(
-        :extras => [
-          { :title => "Instructional Scenarios", :name => "untitled~6320" }
-        ]
-      )
+  use_vcr_cassette :record => :new_episodes
+
+  before(:each) do
+    @extras = Verso::ExtrasList.new(
+      :code => "6320",
+      :edition => Verso::EditionList.new.last.year
     )
   end
 
-  it "responds as Enumerable" do
-    Verso::Extra.should_receive(:new).
-      with(:title => "Instructional Scenarios",
-           :name => "untitled~6320", :code => "6320", :edition => "2011",
-           ).
-      and_return(@extra)
-    @extras_list.first.title.should eq("Instructional Scenarios")
+  describe 'array-like behavior' do
+    describe '#[]' do
+      it 'responds' do
+        @extras.should respond_to(:[])
+      end
+
+      it 'gets a Verso::Extra object' do
+        @extras[1].should be_a(Verso::Extra)
+      end
+    end
+
+    describe '#each' do
+      it 'responds' do
+        @extras.should respond_to(:each)
+      end
+
+      it 'yields' do
+        expect { |b| @extras.each("foo", &b).to yield_control }
+      end
+
+      it 'yields Verso::Extra objects' do
+        @extras.each { |c| c.should be_a(Verso::Extra) }
+      end
+    end
+
+    describe '#empty?' do
+      it 'responds' do
+        @extras.should respond_to(:empty?)
+      end
+
+      it 'is not empty' do
+        @extras.should_not be_empty
+      end
+    end
+
+    describe '#last' do
+      it 'responds' do
+        @extras.should respond_to(:last)
+      end
+
+      it 'is a Verso::Extra object' do
+        @extras.last.should be_a(Verso::Extra)
+      end
+    end
+
+    describe '#length' do
+      it 'responds' do
+        @extras.should respond_to(:length)
+      end
+
+      it 'is a Fixnum' do
+        @extras.length.should be_a(Fixnum)
+      end
+    end
+
+    describe '#first' do
+      it 'responds' do
+        @extras.should respond_to(:first)
+      end
+
+      it 'is a Verso::Extra object' do
+        @extras.first.should be_a(Verso::Extra)
+      end
+    end
+
+    describe '#count' do
+      it 'responds' do
+        @extras.should respond_to(:count)
+      end
+
+      it 'is a Fixnum' do
+        @extras.count.should be_a(Fixnum)
+      end
+    end
   end
 end
