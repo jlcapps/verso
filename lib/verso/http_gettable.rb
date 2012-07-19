da7b117bb038867244f776ff1db37461e28b8cc0
Lee Capps <himself@leecapps.com>
2012-07-19 16:07:29 -0400

    Document Verso::HTTPGettable mixin

--- lib/verso/http_gettable.rb
+++ lib/verso/http_gettable.rb
@@ -1,16 +1,29 @@
 module Verso
+  # Mixin for classes derived from {Verso::Base}. Provides the capability to
+  # fetch the JSON resource to update attributes of the class.
+  #
+  # @abstract Implement a path method that gives the relative path of the
+  #   corresponding JSON resource.
   module HTTPGettable
   protected
 
+    # Get an attribute from the attributes store {#attrs}. Fetch  and merge
+    # first if the attribute store doesn't have the key.
+    #
+    # @param [Symbol] attr The attribute key
+    # @return [Object] The attribute
     def get_attr(attr)
       attrs.merge!(fetch) unless attrs.has_key?(attr)
       super attr
     end
 
+    # HTTP GET the JSON resource
+    # @return [String] JSON resource
     def http_get
       Net::HTTP.get('api.cteresource.org', path, 80)
     end
 
+    # @return [Hash] Resource hash
     def fetch
       JSON.parse(http_get).symbolize_nested_keys!
     end
