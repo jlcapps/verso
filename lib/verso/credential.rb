594352170e7933bcfb990f7ad284c55fe2a7783b
Lee Capps <himself@leecapps.com>
2012-07-19 11:17:43 -0400

    Add attributes to Verso::Credential

--- lib/verso/credential.rb
+++ lib/verso/credential.rb
@@ -1,18 +1,36 @@
 module Verso
   class Credential < Verso::Base
     include HTTPGettable
-    attr_reader :id, :title, :type
+    attr_reader :admin_notes, :amt_seal, :contact_info, :cost, :cte_seal,
+      :description, :has_ancestor, :how_to_earn_it, :id, :items, :pretest,
+      :proctor, :program_area, :retired, :site, :time, :title, :type,
+      :verified_credit
+    alias amt_seal? amt_seal
+    alias cte_seal? cte_seal
+    alias has_ancestor? has_ancestor
+    alias retired? retired
+    alias verified_credit? verified_credit
 
-    def source
-      @source ||= OpenStruct.new(get_attr(:source))
+    def contacts
+      @contacts ||= get_attr(:contacts).collect { |c| OpenStruct.new(c) }
+    end
+
+    def contractor_name
+      get_attr(:contractor_name).to_s
     end
 
     def details
       get_attr(:details).to_s # #to_s b/c API sometimes returns nil
     end
 
+    def source
+      # force update if we only have part of source
+      attrs.merge!(fetch) unless attrs[:source] && attrs[:source].has_key?(:url)
+      OpenStruct.new(get_attr(:source))
+    end
+
     def related_courses
-      get_attr(:related_courses).collect { |rc| Course.new(rc) }
+      @courses ||= get_attr(:related_courses).collect { |rc| Course.new(rc) }
     end
 
   private
