c4199f2a898a177ffcd213d840837eec70864f2b
Lee Capps <himself@leecapps.com>
2012-07-19 15:13:34 -0400

    Add Verso::ExtrasList documentation

--- lib/verso/extras_list.rb
+++ lib/verso/extras_list.rb
@@ -1,5 +1,23 @@
 module Verso
-  class ExtrasList < Verso::Base
+  # Extras List resource
+  #
+  # A collection of {Verso::Extra} associated with a {Course} framework.
+  #
+  # @see http://api.cteresource.org/docs/courses/course/extras
+  #
+  # @example Get extras for 2012 edition of course 6320
+  #   extras = Verso::ExtrasList.new(:code => "6320", :edition => "2012")
+  #
+  # @!attribute [r] code
+  #   @return [String] Course code
+  # @!attribute [r] edition
+  #   @return [String] Edition year
+  #
+  # @overload initialize(attrs={})
+  #   @note Certain attributes are required for instantiation.
+  #   @option attrs [String] :code Course code *Required*
+  #   @option attrs [String] :edition Edition year *Required*
+ class ExtrasList < Verso::Base
     include Enumerable
     include HTTPGettable
     extend Forwardable
