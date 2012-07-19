65a8c2046a311db05b7d966292be69864c2f951a
Lee Capps <himself@leecapps.com>
2012-07-19 12:46:39 -0400

    Add Verso::DutyArea examples

--- spec/duty_area_spec.rb
+++ spec/duty_area_spec.rb
@@ -1,27 +1,57 @@
 require 'spec_helper'
 
 describe Verso::DutyArea do
+  use_vcr_cassette :record => :new_episodes
+
   before do
-    @duty_area = Verso::DutyArea.new(
-      { "code" => "6320",
-        "edition" => "2012",
-        "title" => "A Duty Area",
-        "tasks" => [{ "statement" => "Do this.",
-        "id" => "12345",
-        "sensitive" => false,
-        "essential" => true } ] }
-    )
-    @task = double("Task", :statement => "Do this.", :id => "12345",
-                   :sensitive => false, :essential => true,
-                   :code => "6320", :edition => "2012")
+    @da = Verso::CourseList.new(:code => "6320").last.duty_areas.to_a.last
   end
 
-  it "responds to #title" do
-    @duty_area.title.should eq("A Duty Area")
+  describe '#code' do
+    it "responds" do
+      @da.should respond_to(:code)
+    end
+
+    it 'is a String' do
+      @da.code.should be_a(String)
+    end
+
+    it 'is the code of our course' do
+      @da.code.should == '6320'
+    end
   end
 
-  it "responds to #tasks" do
-    Verso::Task.should_receive(:new).and_return(@task)
-    @duty_area.tasks.first.statement.should eq("Do this.")
+  describe '#edition' do
+    it "responds" do
+      @da.should respond_to(:edition)
+    end
+
+    it 'is a String' do
+      @da.edition.should be_a(String)
+    end
+
+    it 'looks like a year' do
+      @da.edition.should match(/2\d{3}/)
+    end
+  end
+
+  describe '#title' do
+    it "responds" do
+      @da.should respond_to(:title)
+    end
+
+    it 'is a String' do
+      @da.title.should be_a(String)
+    end
+  end
+
+  describe '#tasks' do
+    it "responds" do
+      @da.should respond_to(:tasks)
+    end
+
+    it 'is a collection of Verso::Task objects' do
+      @da.tasks.first.should be_a(Verso::Task)
+    end
   end
 end
