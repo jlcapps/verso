046be2af188bbc51a9361623c0a06b62091c2fb3
Lee Capps <himself@leecapps.com>
2012-07-23 08:49:41 -0400

    Delegate array-like behavior

--- lib/verso/program_area_list.rb
+++ lib/verso/program_area_list.rb
@@ -2,21 +2,15 @@ module Verso
   class ProgramAreaList < Verso::Base
     include Enumerable
     include HTTPGettable
+    extend Forwardable
+    def_delegators :program_areas, :[], :each, :empty?, :last, :length
+
+  private
 
     def program_areas
       @program_areas ||= get_attr(:program_areas).
                            collect { |pa| ProgramArea.new(pa) }
     end
-
-    def each &block
-      program_areas.each &block
-    end
-
-    def last
-      program_areas[-1]
-    end
-
-  private
 
     def path
       "/program_areas/"
