58d5be37e861c8f22be32b3b1f126369ab453b08
Lee Capps <himself@leecapps.com>
2012-07-23 08:49:55 -0400

    Add Verso::ProgramAreaList examples

--- spec/program_area_list_spec.rb
+++ spec/program_area_list_spec.rb
@@ -3,9 +3,83 @@ require 'spec_helper'
 describe Verso::ProgramAreaList do
   use_vcr_cassette :record => :new_episodes
 
-  it 'responds to #first, #last' do
-    list = Verso::ProgramAreaList.new
-    list.first.title.should eq("Academic")
-    list.last.title.should eq("Trade and Industrial Education")
+  before do
+    @list = Verso::ProgramAreaList.new
+  end
+
+  describe '#[]' do
+    it 'responds' do
+      @list.should respond_to(:[])
+    end
+
+    it 'is a Verso::ProgramArea object' do
+      @list[3].should be_a(Verso::ProgramArea)
+    end
+  end
+
+  describe '#each' do
+    it 'responds' do
+      @list.should respond_to(:each)
+    end
+
+    it 'yields' do
+      expect { |b| @list.each("foo", &b).to yield_control }
+    end
+
+    it 'yields Verso::ProgramArea objects' do
+      @list.each do |c|
+        c.should be_a(Verso::ProgramArea)
+      end
+    end
+  end
+
+  describe '#empty?' do
+    it 'responds' do
+      @list.should respond_to(:empty?)
+    end
+
+    it 'is not empty' do
+      @list.should_not be_empty
+    end
+  end
+
+  describe '#last' do
+    it 'responds' do
+      @list.should respond_to(:last)
+    end
+
+    it 'is a Verso::ProgramArea object' do
+      @list.last.should be_a(Verso::ProgramArea)
+    end
+  end
+
+  describe '#length' do
+    it 'responds' do
+      @list.should respond_to(:length)
+    end
+
+    it 'is a Fixnum' do
+      @list.length.should be_a(Fixnum)
+    end
+  end
+
+  describe '#first' do
+    it 'responds' do
+      @list.should respond_to(:first)
+    end
+
+    it 'is a Verso::ProgramArea object' do
+      @list.first.should be_a(Verso::ProgramArea)
+    end
+  end
+
+  describe '#count' do
+    it 'responds' do
+      @list.should respond_to(:count)
+    end
+
+    it 'is a Fixnum' do
+      @list.count.should be_a(Fixnum)
+    end
   end
 end
