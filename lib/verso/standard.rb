6edc430da705f1c27ca8b8cd1ad37597d7a873ee
Lee Capps <himself@leecapps.com>
2012-07-23 09:59:10 -0400

    Add Verso::Standard documentation

--- lib/verso/standard.rb
+++ lib/verso/standard.rb
@@ -1,17 +1,42 @@
 module Verso
+  # Standard resource
+  #
+  # A standard that has been associated with a {Verso::Course}.
+  #
+  # @see http://api.cteresource.org/docs/courses/course/standards/standard
+  #
+  # @!attribute [r] code
+  #   @return [String] Course code
+  # @!attribute [r] edition
+  #   @return [String] Course edition year
+  # @!attribute [r] name
+  #   @return [String] Standard name used as an identifier
+  # @!attribute [r] title
+  #   @return [String] Standard title
+  #
+  # @overload initialize(attrs={})
+  #   @note Any attributes may be set upon instantiation, using Options Hash.
+  #     The following are required:
+  #   @option attrs [String] :code Course code *Required*
+  #   @option attrs [String] :edition Edition year *Required*
+  #   @option attrs [String] :name Standard name (identifier) *Required*
   class Standard < Verso::Base
-    include HTTPGettable
+   include HTTPGettable
     attr_reader :code, :edition, :name, :title
 
+    # @return [String] description
     def description
       get_attr(:description).to_s # sometimes nil
     end
 
+    # @return [Array] collection of goals with #title, #description strings
     def goals
       @goals ||= get_attr(:goals).
         collect { |raw_standard| OpenStruct.new(raw_standard) }
     end
 
+
+    # @return [Array] collection of contained {Verso::Standard} objects
     def sections
       @sections ||= get_attr(:sections).
         collect do |raw_section|
