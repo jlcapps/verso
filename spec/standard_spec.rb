4b091201fcc537e1ee1722d450204c75cbb9653d
Lee Capps <himself@leecapps.com>
2012-07-23 09:35:21 -0400

    Add Verso::Standard examples

--- spec/standard_spec.rb
+++ spec/standard_spec.rb
@@ -1,53 +1,111 @@
 require 'spec_helper'
 
 describe Verso::Standard do
+  use_vcr_cassette :record => :new_episodes
+
   before do
-    @goal = { "title" => "goal title",
-              "description" => "goal description" }
-    @nested_goal = { "title" => "nested goal title",
-                     "description" => "nested goal description" }
-    @section = { "title" => "section title",
-                 "description" => "section description",
-                 "goals" => [@nested_goal], "sections" => [] }
-    @raw_standard =  { :aname => { "title" => "standard title",
-                                   "description" => "standard description",
-                                   "goals" => [@goal],
-                                   "sections" => [@section] } }
-    @standard = Verso::Standard.new(@raw_standard.values.first)
+    @standard = Verso::Standard.new(
+      :code  => "6135",
+      :edition => Verso::EditionList.new.last.year,
+      :name => "english"
+    )
   end
 
-  it "should respond to title" do
-    @standard.title.should eq("standard title")
+  describe '#description' do
+    it 'responds' do
+      @standard.should respond_to(:description)
+    end
+
+    it 'is a string' do
+      @standard.description.should be_a(String)
+    end
   end
 
-  it "should respond to description" do
-    @standard.description.should eq("standard description")
+  describe '#goals' do
+    it 'responds' do
+      @standard.should respond_to(:goals)
+    end
+
+    it 'is an Array' do
+      @standard.goals.should be_a(Array)
+    end
+
+    it 'is an Array of OpenStruct objects' do
+      @standard.goals.first.should be_a(OpenStruct)
+    end
+
+    describe 'OpenStruct goal objects' do
+      before do
+        @goal = @standard.goals.first
+      end
+
+      describe '#title' do
+        it 'responds' do
+          @goal.should respond_to(:title)
+        end
+
+        it 'is a string' do
+          @goal.title.should be_a(String)
+        end
+      end
+
+      describe '#description' do
+        it 'responds' do
+          @goal.should respond_to(:description)
+        end
+
+        it 'is a String' do
+          @goal.description.should be_a(String)
+        end
+
+        it 'looks like HTML' do
+          @goal.description.should match(/<\/.+>/)
+        end
+      end
+    end
   end
 
-  it "should respond to sections" do
-    @standard.sections.first.title.should eq("section title")
+  describe '#name' do
+    it 'responds' do
+      @standard.should respond_to(:name)
+    end
+
+    it 'is a String' do
+      @standard.name.should be_a(String)
+    end
+
+    it 'looks like the title' do
+      @standard.name.should == 'english'
+    end
   end
 
-  it "should respond to goals" do
-    @standard.goals.first.title.should eq("goal title")
+  describe '#sections' do
+    it 'responds' do
+      @standard.should respond_to(:sections)
+    end
+
+    it 'is an Array' do
+      @standard.sections.should be_a(Array)
+    end
+
+    it 'is an Array of Verso::Standard objects' do
+      # english doesn't have subsections, so . . .
+      std = Verso::Standard.new(
+        :code => "8276",
+        :edition => Verso::EditionList.new.last.year,
+        :name => 'fccla_activities--competitive_events'
+      )
+      std.sections.first.should be_a(Verso::Standard)
+    end
   end
 
-  it "should respond to nested goals" do
-    @standard.sections.first.goals.first.title.should eq("nested goal title")
-  end
+  describe '#title' do
+    it 'responds' do
+      @standard.should respond_to(:title)
+    end
 
-  it "should fetch standard as necessary" do
-    course = OpenStruct.new(:code => "6320", :edition => "2011")
-    std = { "name" => "science" }
-    std = Verso::Standard.new(std, course)
-    std.name.should eq("science")
-    std.code.should eq("6320")
-
-    raw_std = { "standard" => { "title" => "Science", "sections" => [],
-                                "goals" => [] } }
-    Net::HTTP.stub(:get).and_return(nil)
-    JSON.stub(:parse).and_return(raw_std)
-    std.title.should eq("Science")
-    std.name.should eq("science")
+    it 'is a String' do
+      @standard.title.should be_a(String)
+    end
   end
 end
