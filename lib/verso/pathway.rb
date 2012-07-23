162dc2c2fcd50b1c32376cd74bce550ef41d2eee
Lee Capps <himself@leecapps.com>
2012-07-23 08:06:07 -0400

    Alpha order

--- lib/verso/pathway.rb
+++ lib/verso/pathway.rb
@@ -3,13 +3,13 @@ module Verso
     include HTTPGettable
     attr_reader :description, :id, :title
 
+    def cluster
+      @cluster ||= Cluster.new(get_attr(:cluster))
+    end
+
     def occupations
       @occupations ||= get_attr(:occupations).
         collect { |o| Occupation.new(o) }
-    end
-
-    def cluster
-      @cluster ||= Cluster.new(get_attr(:cluster))
     end
 
   private
