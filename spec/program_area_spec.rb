53174b57304ac179c958065ef969dc75ea1ca18a
Lee Capps <himself@leecapps.com>
2012-07-23 08:26:17 -0400

    Add Verso::ProgramArea examples

--- spec/program_area_spec.rb
+++ spec/program_area_spec.rb
@@ -1,39 +1,87 @@
 require 'spec_helper'
 
 describe Verso::ProgramArea do
+  use_vcr_cassette :record => :new_episodes
+
   before do
     @pa = Verso::ProgramArea.new("title" => "Career Connections")
   end
 
-  it 'should respond to description' do
-    @pa.description.should eq("")
+  describe '#courses' do
+    it 'responds' do
+      @pa.should respond_to(:courses)
+    end
+
+    it 'is an Array' do
+      @pa.courses.should be_a(Array)
+    end
+
+    it 'is an Array of Verso::Course objects' do
+      @pa.courses.first.should be_a(Verso::Course)
+    end
   end
 
-  it 'should respond to title' do
-    @pa.title.should eq("Career Connections")
+  describe '#deprecated' do
+    it 'responds' do
+      @pa.should respond_to(:deprecated)
+    end
+
+    it 'is Boolean' do
+      @pa.deprecated.to_s.should match(/true|false/)
+    end
   end
 
-  it 'should respond to slug' do
-    @pa.slug.should eq('career-connections')
+  describe '#description' do
+    it 'responds' do
+      @pa.should respond_to(:description)
+    end
+
+    it 'is a String' do
+      @pa.description.should be_a(String)
+    end
   end
 
-  it 'should respond to courses' do
-    @course = double("Course", :title => "a course", :edition => "1998",
-                     :code => "9999")
-    Verso::CourseList.should_receive(:new).
-      with(:program_area => "career connections").
-      and_return([@course])
-    @pa.courses.should eq([@course])
+  describe '#section_overview' do
+    it 'responds' do
+      @pa.should respond_to(:section_overview)
+    end
+
+    it 'is a String' do
+      @pa.section_overview.should be_a(String)
+    end
+
+    it 'is HTML' do
+      @pa.section_overview.should match(/<\/.+>/)
+    end
   end
 
-  it 'should respond to deprecated' do
-    @pa.stub(:http_get).
-      and_return(
-        { "program_areas" => [ {
-            "title" => "Career Connections",
-            "deprecated" => false
-          } ] }.to_json
-      )
-    @pa.deprecated.should eq(false)
+  describe '#slug' do
+    it 'responds' do
+      @pa.should respond_to(:slug)
+    end
+
+    it 'is a string' do
+      @pa.slug.should be_a(String)
+    end
+
+    it 'looks like the title' do
+      @pa.slug.should == 'career-connections'
+    end
+  end
+
+  describe '#title' do
+    it 'responds' do
+      @pa.should respond_to(:title)
+    end
+
+    it 'is a String' do
+      @pa.title.should be_a(String)
+    end
+  end
+
+  describe '#version_date' do
+    it 'responds' do
+      @pa.should respond_to(:version_date)
+    end
   end
 end
