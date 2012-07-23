13076b75fb8f370ca0159a8967fd5ebabda7d62f
Lee Capps <himself@leecapps.com>
2012-07-23 09:35:10 -0400

    Inject attributes, not context

--- lib/verso/standard.rb
+++ lib/verso/standard.rb
@@ -1,29 +1,24 @@
 module Verso
   class Standard < Verso::Base
     include HTTPGettable
-    attr_reader :description, :name, :title
+    attr_reader :code, :edition, :name, :title
 
-    def initialize(attrs, context=nil)
-      @context = context
-      super attrs
-    end
-
-    def code
-      @context && @context.code
-    end
-
-    def edition
-      @context && @context.edition
-    end
-
-    def sections
-      @sections ||= get_attr(:sections).
-        collect { |raw_section| Standard.new(raw_section, @context) }
+    def description
+      get_attr(:description).to_s # sometimes nil
     end
 
     def goals
       @goals ||= get_attr(:goals).
         collect { |raw_standard| OpenStruct.new(raw_standard) }
+    end
+
+    def sections
+      @sections ||= get_attr(:sections).
+        collect do |raw_section|
+          Standard.new(
+            raw_section.merge(:code => code, :edition => edition, :name => name)
+          )
+        end
     end
 
   private
