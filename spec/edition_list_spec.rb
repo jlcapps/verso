8e2e3ba8a45af199a0173a1de2cea449e7be1f05
Lee Capps <himself@leecapps.com>
2012-07-24 17:25:31 -0400

    Use shared examples for array-like behavior

--- spec/edition_list_spec.rb
+++ spec/edition_list_spec.rb
@@ -1,96 +1,24 @@
 require 'spec_helper'
+require 'shared_verso_list_examples'
 
 describe Verso::EditionList do
   use_vcr_cassette :record => :new_episodes
 
   before(:each) do
-    @editions = Verso::EditionList.new
+    @list = Verso::EditionList.new
+    @kontained = OpenStruct
   end
 
-  describe 'array-like behavior' do
-    describe '#[]' do
-      it 'responds' do
-        @editions.should respond_to(:[])
-      end
-
-      it 'gets a OpenStruct object' do
-        @editions[0].should be_a(OpenStruct)
-      end
-    end
-
-    describe '#each' do
-      it 'responds' do
-        @editions.should respond_to(:each)
-      end
-
-      it 'yields' do
-        expect { |b| @editions.each("foo", &b).to yield_control }
-      end
-
-      it 'yields OpenStruct objects' do
-        @editions.each { |c| c.should be_a(OpenStruct) }
-      end
-    end
-
-    describe '#empty?' do
-      it 'responds' do
-        @editions.should respond_to(:empty?)
-      end
-
-      it 'is not empty' do
-        @editions.should_not be_empty
-      end
-    end
-
-    describe '#last' do
-      it 'responds' do
-        @editions.should respond_to(:last)
-      end
-
-      it 'is a OpenStruct object' do
-        @editions.last.should be_a(OpenStruct)
-      end
-    end
-
-    describe '#length' do
-      it 'responds' do
-        @editions.should respond_to(:length)
-      end
-
-      it 'is a Fixnum' do
-        @editions.length.should be_a(Fixnum)
-      end
-    end
-
-    describe '#first' do
-      it 'responds' do
-        @editions.should respond_to(:first)
-      end
-
-      it 'is a OpenStruct object' do
-        @editions.first.should be_a(OpenStruct)
-      end
-    end
-
-    describe '#count' do
-      it 'responds' do
-        @editions.should respond_to(:count)
-      end
-
-      it 'is a Fixnum' do
-        @editions.count.should be_a(Fixnum)
-      end
-    end
-  end
+  it_behaves_like 'any Verso list'
 
   describe 'OpenStruct edition proxy' do
     describe '#year' do
       it 'responds' do
-        @editions.last.should respond_to(:year)
+        @list.last.should respond_to(:year)
       end
 
       it 'looks like a year' do
-        @editions.first.year.should match(/2\d{3}/)
+        @list.first.year.should match(/2\d{3}/)
       end
     end
   end
