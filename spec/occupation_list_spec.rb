8a2b556013af6f9d0d8dd1b374477e7c70781f20
Lee Capps <himself@leecapps.com>
2012-07-25 07:15:18 -0400

    Use shared examples for array-like behavior

--- spec/occupation_list_spec.rb
+++ spec/occupation_list_spec.rb
@@ -1,87 +1,15 @@
 require 'spec_helper'
+require 'shared_verso_list_examples'
 
 describe Verso::OccupationList do
   use_vcr_cassette :record => :new_episodes
 
-  describe 'array-like behavior' do
-    before(:each) do
-      @occupations = Verso::OccupationList.new(:text => "teacher")
-    end
-
-    describe '#[]' do
-      it 'responds' do
-        @occupations.should respond_to(:[])
-      end
-
-      it 'gets a Verso::OccupationData object' do
-        @occupations[10].should be_a(Verso::OccupationData)
-      end
-    end
-
-    describe '#each' do
-      it 'responds' do
-        @occupations.should respond_to(:each)
-      end
-
-      it 'yields' do
-        expect { |b| @occupations.each("foo", &b).to yield_control }
-      end
-
-      it 'yields Verso::OccupationData objects' do
-        @occupations.each { |c| c.should be_a(Verso::OccupationData) }
-      end
-    end
-
-    describe '#empty?' do
-      it 'responds' do
-        @occupations.should respond_to(:empty?)
-      end
-
-      it 'is not empty' do
-        @occupations.should_not be_empty
-      end
-    end
-
-    describe '#last' do
-      it 'responds' do
-        @occupations.should respond_to(:last)
-      end
-
-      it 'is a Verso::OccupationData object' do
-        @occupations.last.should be_a(Verso::OccupationData)
-      end
-    end
-
-    describe '#length' do
-      it 'responds' do
-        @occupations.should respond_to(:length)
-      end
-
-      it 'is a Fixnum' do
-        @occupations.length.should be_a(Fixnum)
-      end
-    end
-
-    describe '#first' do
-      it 'responds' do
-        @occupations.should respond_to(:first)
-      end
-
-      it 'is a Verso::OccupationData object' do
-        @occupations.first.should be_a(Verso::OccupationData)
-      end
-    end
-
-    describe '#count' do
-      it 'responds' do
-        @occupations.should respond_to(:count)
-      end
-
-      it 'is a Fixnum' do
-        @occupations.count.should be_a(Fixnum)
-      end
-    end
+  before(:each) do
+    @list = Verso::OccupationList.new(:text => "teacher")
+    @kontained = Verso::OccupationData
   end
+
+  it_behaves_like 'any Verso list'
 
   it 'returns an empty array if the search is empty' do
     Verso::OccupationList.new.should be_empty
